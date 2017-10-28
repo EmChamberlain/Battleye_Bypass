70172ed96f8f85ed15114fa800fb5b35f7f59ba9
MattChamberlain <Mattchamberlain2@gmail.com>
2017-10-28 00:13:52 -0400

    offsets updated

--- ProcessHandleHijack/ProcessHandleHijack/GameDataParser.hpp
+++ ProcessHandleHijack/ProcessHandleHijack/GameDataParser.hpp
@@ -206,7 +206,7 @@ private:
 
 	void readLocals()
 	{
-		m_UWorld = m_kReader->readType64(m_kReader->getPUBase() + 0x37E4918, PROTO_NORMAL_READ);
+		m_UWorld = m_kReader->readType64(m_kReader->getPUBase() + 0x37e5988, PROTO_NORMAL_READ);
 		m_gameInstance = m_kReader->readType64(m_UWorld + 0x140, PROTO_NORMAL_READ);
 		m_ULocalPlayer = m_kReader->readType64(m_gameInstance + 0x38, PROTO_NORMAL_READ);
 		m_localPlayer = m_kReader->readType64(m_ULocalPlayer, PROTO_NORMAL_READ);
