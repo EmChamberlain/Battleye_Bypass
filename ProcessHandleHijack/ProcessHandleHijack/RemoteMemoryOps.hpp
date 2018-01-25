0f68b436e0def4d36beae847a670eb5dcba3bfc6
MattChamberlain <Mattchamberlain2@gmail.com>
2018-01-25 02:42:53 -0500

    updated offsets

--- ProcessHandleHijack/ProcessHandleHijack/RemoteMemoryOps.hpp
+++ ProcessHandleHijack/ProcessHandleHijack/RemoteMemoryOps.hpp
@@ -18,9 +18,9 @@ extern std::vector<std::string> playerGNameVec;
 extern std::vector<std::string> vehicleGNameVec;
 extern std::map<std::string, std::string> dropGNameMap;
 
-#define UWORLD 0x4041430 // sig = 48 89 05 ? ? ? ? 0F 28 D6
-#define GNAMES 0x3F238C8 // sig = 75 BB 48 8B 35 ? ? ? ? 41 0F B7 C4
-//#define ETABLE 0x3F0C080  //GObjsAddress ? // sig = 48 8d 0d ? ? ? ? c6 05 ? ? ? ? 01 e8 ? ? ? ? c6
+#define UWORLD 0x40546F0 // sig = 48 89 05 ? ? ? ? 0F 28 D6
+#define GNAMES 0x3F36B48 // sig = 75 BB 48 8B 35 ? ? ? ? 41 0F B7 C4
+//#define ETABLE 0x3F3F500  //GObjsAddress // sig = 48 8d 0d ? ? ? ? c6 05 ? ? ? ? 01 e8 ? ? ? ? c6
 
 #define PIPEWAITTIMOUTIFBUSY 20000
 
